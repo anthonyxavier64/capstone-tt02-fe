8ad131d05fdd4e027548c0a2a1e55c76b489c54f
Marcus Ng <user@MacBook-Air-2.local>
2021-09-09 16:14:40 +0800

    [Feature] Reorganization of Navbar and Addition of Work Drop List

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
+  @ViewChild('clickHoverMenuTrigger') clickHoverMenuTrigger: MatMenuTrigger;
 
-  ngOnInit(): void {
+  openOnMouseOver() {
+    this.clickHoverMenuTrigger.openMenu();
   }
-
 }
