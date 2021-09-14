74497d9fd4e8d88e307974a7ca006296a81a093d
PhireHandy <taha.jailani@gmail.com>
2021-09-15 00:51:13 +0800

    Created getAllUsers service
    Created getAllUsers service
    Set up the UI for Employee Management
    
    TODO:
    Implement template and components for csv processing
    Fix the getAllUsers service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './guards/auth.guard';
-import { AdminComponent } from './pages/admin/adminLanding/admin.component';
+
 import { AdminAnnouncementManagementComponent } from './pages/admin/announcementManagement/adminAnnouncementManagement.component';
 import { AdminCompanyDetailsManagementComponent } from './pages/admin/companyDetailsManagement/adminCompanyDetailsManagement.component';
+import { AdminComponent } from './pages/admin/adminLanding/admin.component';
 import { AdminEmployeeManagementComponent } from './pages/admin/employeeManagement/adminEmployeeManagement.component';
 import { AdminWfoManagementComponent } from './pages/admin/wfoManagement/adminWfoManagement.component';
+import { AuthGuard } from './guards/auth.guard';
 import { IndexComponent } from './pages/index/index.component';
+import { NgModule } from '@angular/core';
 import { ProfileComponent } from './pages/profile/profile.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/index', pathMatch: 'full' },
   { path: 'index', component: IndexComponent },
-  { path: 'admin', component: AdminComponent, canActivate: [AuthGuard] },
+  {
+    path: 'admin',
+    component: AdminComponent,
+    canActivate: [AuthGuard],
+  },
   {
     path: 'adminCompanyDetailsManagement',
     component: AdminCompanyDetailsManagementComponent,
