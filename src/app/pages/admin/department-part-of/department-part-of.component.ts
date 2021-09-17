c595ae76533b8831e1d8b921bed001d094401525
PhireHandy <taha.jailani@gmail.com>
2021-09-17 14:01:49 +0800

    Generated Dialogs
    Dialogs for Employee CRUD generated, linked to adminEmployeeManagement

---
+++ src/app/pages/admin/department-part-of/department-part-of.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-department-part-of',
+  templateUrl: './department-part-of.component.html',
+  styleUrls: ['./department-part-of.component.css']
+})
+export class DepartmentPartOfComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
