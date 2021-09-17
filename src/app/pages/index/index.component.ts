6f1ba1c3bd716dc4db7f41e344b2d5c9aade7eb2
PhireHandy <taha.jailani@gmail.com>
2021-09-17 23:31:37 +0800

    temporary push

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,24 +1,34 @@
-import { AfterViewInit, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+} from '@angular/core';
 
 import { AuthService } from 'src/app/services/user/auth.service';
+import { Department } from 'src/app/models/department.model';
 import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css']
+  styleUrls: ['./index.component.css'],
 })
 export class IndexComponent implements OnInit, AfterViewInit {
   email: string;
   password: string;
 
-  constructor(private cdRef: ChangeDetectorRef, private auth: AuthService, private router: Router) {
+  constructor(
+    private cdRef: ChangeDetectorRef,
+    private auth: AuthService,
+    private router: Router
+  ) {
     this.email = '';
     this.password = '';
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   ngAfterViewInit(): void {
     this.cdRef.detectChanges();
@@ -26,17 +36,85 @@ export class IndexComponent implements OnInit, AfterViewInit {
 
   handleLogin() {
     this.auth.login(this.email, this.password).then(
-      response => {
+      (response) => {
         localStorage.setItem('currentUser', JSON.stringify(response));
         this.router.navigateByUrl('/admin');
+
+        // The following code is only when DB does not work (ie need to cache).
+        const allUsers = [
+          {
+            id: 1,
+            fullName: 'First User',
+            email: '1@test.com',
+            password:
+              '$2a$10$O.FiOY9TAl.qi.zYqa.qsuIGesVsHXjtrFHl85t2jz3X4YWsFpPQG',
+            contactNumber: '12345678',
+            isVaccinated: true,
+            isActivated: true,
+            numLeavesAllocated: 14,
+            numLeavesTaken: 2,
+            datesInOffice: JSON.stringify([]),
+            shnMedicalCerts: JSON.stringify([]),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            id: 2,
+            fullName: 'Second User',
+            email: '2@test.com',
+            password:
+              '$2a$10$O.FiOY9TAl.qi.zYqa.qsuIGesVsHXjtrFHl85t2jz3X4YWsFpPQG',
+            contactNumber: '12345678',
+            isVaccinated: true,
+            isActivated: true,
+            numLeavesAllocated: 14,
+            numLeavesTaken: 2,
+            datesInOffice: JSON.stringify([]),
+            shnMedicalCerts: JSON.stringify([]),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            id: 3,
+            fullName: 'Third User',
+            email: '3@test.com',
+            password:
+              '$2a$10$O.FiOY9TAl.qi.zYqa.qsuIGesVsHXjtrFHl85t2jz3X4YWsFpPQG',
+            contactNumber: '12345678',
+            isVaccinated: true,
+            isActivated: true,
+            numLeavesAllocated: 14,
+            numLeavesTaken: 2,
+            datesInOffice: JSON.stringify([]),
+            shnMedicalCerts: JSON.stringify([]),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        ];
+
+        const allDepts = [
+          {
+            id: 1,
+            name: 'HR',
+          },
+          {
+            id: 2,
+            name: 'Finance',
+          },
+          {
+            id: 2,
+            name: 'ICT',
+          },
+        ];
+
+        localStorage.setItem('allUsers', JSON.stringify(allUsers));
+        localStorage.setItem('allDepts', JSON.stringify(allDepts));
       },
-      error => {
+      (error) => {
         console.log(error);
       }
-    )
+    );
   }
 
-  handleLoginPopup() {
-    
-  }
+  handleLoginPopup() {}
 }
