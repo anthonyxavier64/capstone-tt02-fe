7f5c33fbcade347d5083bfe6f0648a952ff898ad
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 <66234273+yappeizhen@users.noreply.github.com>
2021-09-18 17:14:00 +0800

    Resolved merge conflict:

---
+++ src/app/services/company/company.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment.dev';
+import { handleError } from '../services-util';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CompanyService {
+  baseUrl: string = `${environment.API_REST_URL}`;
+
+  constructor(private httpClient: HttpClient) {}
+
+  updateCompany() {}
+}
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,27 +1,39 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { User } from '../../models/user';
-import { handleError } from '../services-util';
 import { environment } from 'src/environments/environment.dev';
 
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { User } from '../../models/user';
+import { handleError } from '../services-util';
+
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
 };
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-  baseUrl: string = environment.API_REST_URL + "/user";
+  baseUrl: string = environment.API_REST_URL + '/user';
 
-  constructor(private httpClient: HttpClient) {
-  }
+  constructor(private httpClient: HttpClient) {}
 
-  getUsers(): Observable<User[]> {
+  getUsers(): Observable<any[]> {
+    console.log('getUsersService');
+    // this.httpClient
+    //   .get<any>(this.baseUrl + '/get-all-users')
+    //   .pipe(catchError(handleError))
+    //   .subscribe((response) => {
+    //     console.log(response[0].fullName);
+    //   });
     return this.httpClient
-      .get<any>(this.baseUrl + "/get-all-users")
+      .get<any[]>(this.baseUrl + '/get-all-users')
       .pipe(catchError(handleError));
   }
 
