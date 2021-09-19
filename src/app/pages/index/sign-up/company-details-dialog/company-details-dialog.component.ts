6f6d0b7b9e26405354233e083ec401ad529b64af
anthonyxavier64 <e0406854@u.nus.edu>
2021-09-20 00:53:19 +0800

    refactor: naming of attributes

--- src/app/pages/index/sign-up/company-details-dialog/company-details-dialog.component.ts
+++ src/app/pages/index/sign-up/company-details-dialog/company-details-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CompanyService } from 'src/app/services/company/company.service';
 import { CompanyDetailsDialogData } from '../sign-up.component';
 
 @Component({
@@ -8,15 +9,26 @@ import { CompanyDetailsDialogData } from '../sign-up.component';
   styleUrls: ['./company-details-dialog.component.css'],
 })
 export class CompanyDetailsDialogComponent implements OnInit {
+  company: any | undefined;
+
   constructor(
     public dialogRef: MatDialogRef<CompanyDetailsDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: CompanyDetailsDialogData
-  ) {}
+    @Inject(MAT_DIALOG_DATA) public data: CompanyDetailsDialogData,
+    private companyService: CompanyService
+  ) {
+    this.company = data;
+  }
 
   ngOnInit(): void {}
 
   onSignUpClick(): void {
     // Pass data to back end from here
+
+    this.companyService.createCreationRequest(this.company).subscribe(
+      (response) => {},
+      (error) => {}
+    );
+
     this.dialogRef.close();
   }
 
