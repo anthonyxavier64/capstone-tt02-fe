c81143d0ae92cde327c1143af7fef30f392a0420
eviehwy <e0323118@u.nus.edu>
2021-09-26 02:55:46 +0800

    admin guide in notifs

--- src/app/sharedComponents/navbar/navbar.component.ts
+++ src/app/sharedComponents/navbar/navbar.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatMenuTrigger } from '@angular/material/menu';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/user/auth.service';
+import { MatDialog } from '@angular/material/dialog';
+
+import { AdminGuideComponent } from 'src/app/pages/admin/adminLanding/admin-guide/admin-guide.component';
 
 @Component({
   selector: 'app-navbar',
@@ -9,9 +12,9 @@ import { AuthService } from 'src/app/services/user/auth.service';
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-  constructor(private router: Router, private auth: AuthService) {}
+  constructor(private router: Router, private auth: AuthService, public dialog: MatDialog) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
   @ViewChild('clickHoverMenuTrigger') clickHoverMenuTrigger: MatMenuTrigger;
 
   onCompanyLogoClick() {
@@ -27,5 +30,15 @@ export class NavbarComponent implements OnInit {
     this.auth.logout();
     this.router.navigateByUrl('/index');
     alert('Thank you for using Flexiwork, ' + currentUser.fullName + '!');
+  }
+
+  openDialog(): void {
+    let dialogRef = this.dialog.open(AdminGuideComponent, {
+      data: 'test'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      // this.message = result;
+    });
   }
 }
