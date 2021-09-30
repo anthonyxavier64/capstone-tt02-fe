05956e0bf8cbed37b8dbf7732e5211c17043fb04
eviehwy <e0323118@u.nus.edu>
2021-10-01 05:27:18 +0800

    progress circle and dates done

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,16 +1,79 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { DatePipe } from '@angular/common';
+import * as moment from 'moment';
+
+import { UserService } from 'src/app/services/user/user.service';
+
+import { Announcement } from 'src/app/models/announcement';
+import { ViewAnnouncementComponent } from '../view-announcement/view-announcement.component';
+import { AnnouncementType } from 'src/app/models/announcement-type';
+import { AnnouncementService } from 'src/app/services/announcement/announcement.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
+  providers: [DatePipe]
 })
 export class DashboardComponent implements OnInit {
+  user: any | null;
 
-  constructor(private router: Router) { }
+  covidAnnouncements: Announcement[];
+  generalAnnouncements: Announcement[];
 
-  ngOnInit(): void {
+  today = new Date();
+  weekday = this.datePipe.transform(this.today, "EEEE");
+  startDate = moment().startOf('week').toDate();
+  endDate = moment().endOf('week').toDate();
+  
+  constructor(private router: Router, 
+    private userService: UserService, 
+    private announcementService: AnnouncementService,
+    private matDialog: MatDialog,
+    private datePipe: DatePipe) 
+    { let now = moment(); }
+
+  ngOnInit() {
+    const currentUser = localStorage.getItem('currentUser');
+    if (currentUser) {
+      this.user = JSON.parse(currentUser);
+    }
+
+    this.announcementService.getCovidAnnouncements(this.user.userId).subscribe(
+      response => {
+        this.covidAnnouncements = response.announcements;
+      },
+      error => {
+        console.log('********** AdminAnnouncementManagementComponent.ts: ' + error);
+      }
+    );
+
+    this.announcementService.getGeneralAnnouncements(this.user.userId).subscribe(
+      response => {
+        this.generalAnnouncements = response.announcements;
+      },
+      error => {
+        console.log('********** AdminAnnouncementManagementComponent.ts: ' + error);
+      }
+    );
+
+  }
+
+  viewAnnouncement(announcement?: Announcement) {
+    const viewDialog = this.matDialog.open(ViewAnnouncementComponent, {
+      data: {
+        title: announcement?.title,
+        date: announcement?.date,
+        description: announcement?.description,
+      }
+    });
+    viewDialog.afterClosed().subscribe(result => {
+      if (result === false) {
+        return;
+      }
+    });
   }
 
 }
