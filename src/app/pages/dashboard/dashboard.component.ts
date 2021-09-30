163d42a9276392449aa092154d8c45b1ae52c9e3
eviehwy <e0323118@u.nus.edu>
2021-09-30 11:34:02 +0800

    announcement tab done

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+
 import { UserService } from 'src/app/services/user/user.service';
+
+import { Announcement } from 'src/app/models/announcement';
+import { ViewAnnouncementComponent } from '../view-announcement/view-announcement.component';
+import { AnnouncementType } from 'src/app/models/announcement-type';
+import { AnnouncementService } from 'src/app/services/announcement/announcement.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,13 +17,53 @@ import { UserService } from 'src/app/services/user/user.service';
 export class DashboardComponent implements OnInit {
   user: any | null;
 
-  constructor(private router: Router, private userService: UserService) { }
+  covidAnnouncements: Announcement[];
+  generalAnnouncements: Announcement[];
+
+  constructor(private router: Router, 
+    private userService: UserService, 
+    private announcementService: AnnouncementService,
+    private matDialog: MatDialog) { }
 
   ngOnInit() {
     const currentUser = localStorage.getItem('currentUser');
     if (currentUser) {
       this.user = JSON.parse(currentUser);
     }
+
+    this.announcementService.getCovidAnnouncements(this.user.userId).subscribe(
+      response => {
+        this.covidAnnouncements = response.announcements;
+      },
+      error => {
+        console.log('********** AdminAnnouncementManagementComponent.ts: ' + error);
+      }
+    );
+
+    this.announcementService.getGeneralAnnouncements(this.user.userId).subscribe(
+      response => {
+        this.generalAnnouncements = response.announcements;
+      },
+      error => {
+        console.log('********** AdminAnnouncementManagementComponent.ts: ' + error);
+      }
+    );
+
+  }
+
+  viewAnnouncement(announcement?: Announcement) {
+    const viewDialog = this.matDialog.open(ViewAnnouncementComponent, {
+      data: {
+        title: announcement?.title,
+        date: announcement?.date,
+        description: announcement?.description,
+      }
+    });
+    viewDialog.afterClosed().subscribe(result => {
+      if (result === false) {
+        return;
+      }
+    });
   }
 
 }
