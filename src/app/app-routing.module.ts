e573c990ef10574beab9a2b7371f75a457b162a2
eviehwy <e0323118@u.nus.edu>
2021-10-10 23:15:36 +0800

    basic calendar template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,6 +19,7 @@ import { IndexComponent } from './pages/index/index.component';
 import { LoginComponent } from './pages/index/login/login.component';
 import { SignUpComponent } from './pages/index/sign-up/sign-up.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { CalendarComponent } from './pages/calendar/calendar.component';
 
 const user = JSON.parse(localStorage.getItem('currentUser'));
 
