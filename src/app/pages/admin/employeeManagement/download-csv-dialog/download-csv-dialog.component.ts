9fd7ff4eca96e14e1ca7ab5a34c400189dc2f61f
Marcus Ng <user@MacBook-Air-2.local>
2021-10-12 03:11:46 +0800

    feat(adminemployeemanagement): wip - upload, download complete, need read data

--- src/app/pages/admin/employeeManagement/download-csv-dialog/download-csv-dialog.component.ts
+++ src/app/pages/admin/employeeManagement/download-csv-dialog/download-csv-dialog.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 
 @Component({
   selector: 'app-download-csv-dialog',
   templateUrl: './download-csv-dialog.component.html',
-  styleUrls: ['./download-csv-dialog.component.css']
+  styleUrls: ['./download-csv-dialog.component.css'],
 })
 export class DownloadCsvDialogComponent implements OnInit {
+  company: any | undefined;
+  templateDownloadUrl: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    public ref: DynamicDialogRef,
+    public config: DynamicDialogConfig
+  ) {
+    this.company = this.config.data.company;
+    this.templateDownloadUrl = this.config.data.downloadLink;
   }
 
+  ngOnInit(): void {}
+
+  downloadNewCsv(): void {
+    window.open(this.templateDownloadUrl, '_self');
+  }
+
+  downloadExistingCsv(): void {
+    window.open(this.company.employeeCsv, '_self');
+  }
 }
