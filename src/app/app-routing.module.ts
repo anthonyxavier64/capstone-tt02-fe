7b8ee5ae623409fb07a38ed9df509c387d23ed15
PhireHandy <taha.jailani@gmail.com>
2021-10-15 15:02:20 +0800

    Set up FE for createNewMeeting
    Todo:
    1) Misc bug fixes
    2) Link up FE to BE
    3) Implement new control flow (along with graying out of sections)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { AuthGuard } from './guards/auth.guard';
 import { AdminComponent } from './pages/admin/adminLanding/admin.component';
 import { AdminAnnouncementManagementComponent } from './pages/admin/announcementManagement/adminAnnouncementManagement.component';
 import { AdminCompanyDetailsManagementComponent } from './pages/admin/companyDetails/companyDetailsManagement/adminCompanyDetailsManagement.component';
 import { OfficeSpaceConfigComponent } from './pages/admin/companyDetails/office-space-config/office-space-config.component';
-import { EditAnnouncementComponent } from './pages/admin/edit-announcement/edit-announcement.component';
 import { AdminEmployeeManagementComponent } from './pages/admin/employeeManagement/adminEmployeeManagement.component';
 import { AdminWfoManagementComponent } from './pages/admin/wfoManagement/adminWfoManagement.component';
 import { AlternateWorkTeamsConfigComponent } from './pages/admin/wfoManagement/alternateWorkTeamsConfig/alternateWorkTeamsConfig.component';
 import { OfficeQuotaConfigComponent } from './pages/admin/wfoManagement/officeQuotaConfig/officeQuotaConfig.component';
+import { CalendarComponent } from './pages/calendar/calendar.component';
 import { CovidDeclarationsComponent } from './pages/covid-declarations/covid-declarations.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { CreateNewMeetingComponent } from './pages/employee/meeting/create-new-meeting/create-new-meeting.component';
 import { TaskComponent } from './pages/employee/task/task.component';
 import { IndexComponent } from './pages/index/index.component';
 import { LoginComponent } from './pages/index/login/login.component';
 import { SignUpComponent } from './pages/index/sign-up/sign-up.component';
 import { ProfileComponent } from './pages/profile/profile.component';
-import { CalendarComponent } from './pages/calendar/calendar.component';
 
 const user = JSON.parse(localStorage.getItem('currentUser'));
 
@@ -94,10 +93,15 @@ const routes: Routes = [
     canActivate: [AuthGuard],
   },
   {
-    path:'calendar',
+    path: 'calendar',
     component: CalendarComponent,
     canActivate: [AuthGuard],
-  }
+  },
+  {
+    path: 'create-meeting',
+    component: CreateNewMeetingComponent,
+    canActivate: [AuthGuard],
+  },
 ];
 
 @NgModule({
