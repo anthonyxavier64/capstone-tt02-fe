3d6782c7948834e949206f08a1953238859a8058
sthitiacharya <sthitiacharya@gmail.com>
2021-11-15 10:04:24 +0800

    [Productivity] added search by dept and made some styling changes

--- src/app/pages/admin/productivity-tracking/productivity-management/productivity-management.component.ts
+++ src/app/pages/admin/productivity-tracking/productivity-management/productivity-management.component.ts
@@ -9,11 +9,7 @@ import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { ViewProductivityDialogComponent } from '../view-productivity-stats/view-productivity-dialog/view-productivity-dialog.component';
 
 export interface user {
-<<<<<<< HEAD
   userId: string;
-=======
-  userId: number;
->>>>>>> 2729b6b ([Productivity] My Employees Page, View Employee Details Dialog Box & Productivity Calc done)
   fullName: string;
   email: string;
   createdAt: string;
@@ -80,12 +76,6 @@ export class ProductivityManagementComponent implements OnInit {
         }
       );
     }
-<<<<<<< HEAD
-
-=======
-    // Below is the correct code
-    // this.isLoading = false;
->>>>>>> 2729b6b ([Productivity] My Employees Page, View Employee Details Dialog Box & Productivity Calc done)
     this.userService.getUsers(this.user.companyId).subscribe(
       (response) => {
         this.allUsers = response.users;
@@ -93,15 +83,11 @@ export class ProductivityManagementComponent implements OnInit {
         this.isLoading = false;
       },
       (error) => {
-<<<<<<< HEAD
         this.messageService.add({
           severity: 'error',
           summary: 'Error',
           detail: 'Could not retrieve users',
         });
-=======
-        console.log(error);
->>>>>>> 2729b6b ([Productivity] My Employees Page, View Employee Details Dialog Box & Productivity Calc done)
       }
     );
   }
@@ -132,20 +118,15 @@ export class ProductivityManagementComponent implements OnInit {
       },
       (error) => {
         this.isLoading = false;
-<<<<<<< HEAD
         this.messageService.add({
           severity: 'error',
           summary: 'Error',
           detail: 'Could not retrieve users',
         });
-=======
-        console.log(error);
->>>>>>> 2729b6b ([Productivity] My Employees Page, View Employee Details Dialog Box & Productivity Calc done)
       }
     );
   }
 
-<<<<<<< HEAD
   handleDepartmentSelection() {
     if (!!this.selectedDepartment) {
       this.isLoading = true;
@@ -198,8 +179,6 @@ export class ProductivityManagementComponent implements OnInit {
     }
   }
 
-=======
->>>>>>> 2729b6b ([Productivity] My Employees Page, View Employee Details Dialog Box & Productivity Calc done)
   openViewEmployeeDialog(chosenUser: {
     userId: number;
     fullName: string;
